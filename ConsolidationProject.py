8ba046d3f940f05e7d2bac4763947d93802d8f2f
Aaron Gross <aar010n@terpmail.umd.edu>
2024-11-27 18:00:16 -0500

    Createed rough outline of Decktionary Battle

---
+++ ConsolidationProject.py
@@ -0,0 +1,11 @@
+#Import random
+#First step, start setting up the decktionary battle deck (MAKE SURE TO EXCLUDE KINGS)
+#Players must now get their hands
+#Show the players hand to them so they can play
+#Decide whether of not player one or player two won the hand
+#Create the game loop
+#Player one will start
+#Player 2 will play after player 1
+#Decide who won
+#Final score and winner of Decktionary battle
+#Run game
