1c5045468064f668f1f8ff19817c6f1360709ee8
Aaron Gross <aar010n@terpmail.umd.edu>
2024-11-27 18:22:09 -0500

    Created base of deck and added suits and values

--- ConsolidationProject.py
+++ ConsolidationProject.py
@@ -1,6 +1,9 @@
 #Import random
 import random
 #First step, start setting up the decktionary battle deck (MAKE SURE TO EXCLUDE KINGS)
+def create_deck():
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    values = list(range(1, 14))  #Range excluding the king
 #Players must now get their hands
 #Show the players hand to them so they can play
 #Decide whether of not player one or player two won the hand
